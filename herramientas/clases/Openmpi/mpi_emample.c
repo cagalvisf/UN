7848533fe5fa8be069b5b60da5bb7f415a1eaaa8
cagalvisf <cagalvisf@unal.edu.co>
2017-10-25 10:50:10 -0500

    Clase 25-10-2017

---
+++ herramientas/clases/Openmpi/mpi_emample.c
@@ -0,0 +1,22 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  int processId;                             /* rank of process */
+  int noProcesses;                           /* number of processes */
+  int nameSize;                              /* lenght of name */
+  char computerName[MPI_MAX_PROCESSOR_NAME]; 
+
+  MPI_Init(&argc, &argv);                                       /* Mandatory */
+  MPI_Comm_size(MPI_COMM_WORLD, &noProcesses);                  /* cuantos hay */
+  MPI_Comm_rank(MPI_COMM_WORLD, &processId);                    /*Quien soy*/
+
+  MPI_Get_processor_name(computerName, &nameSize);
+  fprintf(stderr, "Hello from process %d of %d, on %s\n", processId, noProcesses, computerName);
+
+  MPI_Finalize();                                               /* Mandatory */
+
+  return 0; 
+}          
+
