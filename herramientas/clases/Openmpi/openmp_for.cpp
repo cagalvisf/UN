61755cb985901136cd005d67c7f747499b629b7c
cagalvisf <cagalvisf@unal.edu.co>
2017-10-25 09:17:36 -0500

    Opem mpi

---
+++ herramientas/clases/Openmpi/openmp_for.cpp
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  const int N = 100000;
+  int i, a[N];
+
+#pragma omp parallel for
+  for(i = 0; i < N; i++)
+    a[i] = 2*i;
+  
+  return 0;
+}
