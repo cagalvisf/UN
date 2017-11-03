14d379386a2eccb803a43af47816f7e4275ff0ff
cagalvisf <cagalvisf@unal.edu.co>
2017-11-03 10:54:38 -0500

    potencial

--- herramientas/clases/PartialDE/laplace.cpp
+++ herramientas/clases/PartialDE/laplace.cpp
@@ -5,23 +5,29 @@
 const double L = 1.479;
 const double delta = 0.1;
 const int N = 1 + int(L/delta);
-const int steps = 100;
+const int steps = 400;
 
 typedef std::vector<double> Matrix;
 
 void print (const Matrix & m);
 void boundary_conditions(Matrix & m);
+void charge_density(Matrix & m);
 void initial_conditions(Matrix & m);
+void plot_gnuplot(const Matrix & m);
+void init_gnuplot(void);
 void evolve(Matrix & m);
 int main(){
+  init_gnuplot();
  
   Matrix array(N*N);
   
   initial_conditions(array);
   boundary_conditions (array);
-  
+  charge_density(array);
   for(int ii = 0; ii<=steps; ii++){
-    evolve(array);}
+    evolve(array);
+     plot_gnuplot(array);
+  }
 
   print(array);
  
@@ -65,6 +71,7 @@ void evolve(Matrix & m){
       if(ii == N-1) continue;
       if(jj == 0) continue;
       if(jj == N-1) continue;
+      if (ii == N-7 && (2 <= jj < N-2)) continue;
 
 
       m[ii*N + jj]= (m[(ii + 1)*N + jj] +
@@ -82,4 +89,25 @@ void print (const Matrix & m){
     }
     std::cout<< "\n";
   }
+}
+void charge_density(Matrix & m){
+  int ii= 0, jj=0;
+  ii = N-7;
+  for (jj = 2; jj < N-2; ++jj)
+    m[ii*N + jj]=42;
+
+}
+
+void init_gnuplot(void)
+{
+  std::cout << "set contour " << std::endl;
+  std::cout << "set terminal gif animate " << std::endl;
+  std::cout << "set out 'anim.gif' " << std::endl;
+}
+
+void plot_gnuplot(const Matrix & m)
+{
+  std::cout << "splot '-' w pm3d " << std::endl;
+  print(m);
+  std::cout << "e" << std::endl;  
 }
